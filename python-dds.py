ed41f3ed757d9ea635e7e3c8d609a2739f577fdc
Khojiakbar <usmondev@outlook.kr>
2024-09-23 05:22:00 +0500

    BCC

---
+++ python-dds.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 17 08:40:27 2024
+
+@author: Usmonov Hojiakbar
+"""
+
+
+#print ("Hello World!")
+#print("Assalomu Alekum")
+#Print Funksiyasi
+#print("""Odami ersang demagil odami,
+#print("""Oning kim yo'qdur halq g'amidan g'ami""")
+#print("Odami ersang \ndemagil odami,\nOning kim yo'qdur halq g'amidan g'ami")
+#print('Odami ersang \ndemagil odami,\nOning kim yo\'qdur halq g\'amidan g\'ami')
+#print("Hayrli Tong")
+#print("""Bugun havo,
+#qanday""")
+#print("Bugun \nErta \nIndin")
+#print('og\'riq qayg\'u')
+#print("Hayrli Tong")
+#print('Men "Dell" markasidagi notebook sotib oldim')
+#print(2+4*2)
+#print(19/5)
+#print(20/5)
+#print(16//4)
+#print(10//3)
+#print(2**3)
+#print(50**50)
+#print(100%28)
+#print("To'qqizni kvadrati", 9**2, "ga teng")
+#print('3x3=',3*3)
+#print(2*5*3.14159)
+#Radiusi 5 ga teng bo'lgan aylananing uzunligi quyidagicha hisoblanadi
+#print(2*5*3.14159)
+#print("Assalom alaykum!") # Bu matn konsolda chiqadi
+#Keyingi qator esa bajarilmaydi
+#print("Mening ismim Anvar")
+#print("\"Nexia\",\"Tico\", \'Damas\' ko\'rganlar qilar havas")
+#print(5**4)
+#print(22/4)
+#print(4*125)
+
+#04-Dars O'zgaruvchilar
+#ism = "Abdulloh"
+#yosh = 25
+#print(ism)
+#print(yosh)
+
+#ism = "Abdulloh"
+#print(ism)
+#ism = "Muhammad"
+#print(ism)
+
+#a = 6
+#b = 5
+#c = (a+b)**2
+#print(c)
+
+#a = 'Hello '
+#b = 'World'
+#print(a + b)
+
+#xabar = "Assalomu alekum sohtalar"
+#print(xabar)
+#xabar = "ex ex soxtalar"
+#print(xabar)
+
+#radius = 5
+#pi = 3.14159
+#aylana_yuzi = pi * radius**2
+#print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
+
+
+#05-Dars String (Matn)
+#salom = '😁'
+#print(salom)
+
+#ism = 'Ahad'
+#familya = 'Qayumov'
+#ism_sharif = f"{ism} {familya}"
+#print(ism_sharif)
+#print(f"Salom, Mening ismim {familya}, {ism} {familya}")
+#ism_sharif = ism_sharif.upper()
+#print(ism_sharif.upper())
+#print(ism_sharif.lower())
+#print(ism_sharif.title())
+#print(ism_sharif.capitalize())
+#meva = "     Olma     "
+#print(meva)
+#print("Men " + meva.lstrip() + "yaxshi ko'raman")
+#print("Men " + meva.strip() + " yaxshi ko'raman")
+#print("Men " + meva + "yaxshi ko'raman")
+
+#ism = input("Ismingiz nima? ")
+#print("Assalomu alekum, " + ism)
+
+#ism = input("Ismingiz nima?\n>>>") #Foydalanuvchi ismi yangi qatordan
+#print("Assalomu alekum, " + ism.title())
+
+#kocha = "Anorzor"
+#mahalla = "Quyi Rovuston"
+#tuman = "Davlatobod"
+#viloyat = "Namangan"
+print(f"{kocha} ko'cha, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati")
+print("Iltimos, quyidagi ma'lumotlarni kiriting:")
+kocha = input("Ko'changiz?\n>>>")
+mahalla = input("Mahallangiz?\n>>>")
+tuman = input("Tumaningiz?\n>>>")
+viloyat = input("Viloyatingiz?\n>>>")
+print(f"Assalomu alekum,  {kocha.title()} ko'cha, {mahalla.title()} mahalla, {tuman.title()} tuman, {viloyat.title()} viloyat")
+#print(kocha + " ko'chasi, " + mahalla + " mahallasi, " + \
+     # tuman + " tumani, " + viloyat + " viloyati")
+manzil = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+print(manzil)
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.title())
+print(manzil.capitalize())
+print(manzil.capitalize())
+
+
+
+
+
+#print('Hello World')
+#print('Hello \tWorld')
+#print('Hello \nWorld')
