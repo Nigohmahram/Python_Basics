54ea3f8a46388aa6ea128ae46560fce3d0224d26
Khojiakbar <usmondev@outlook.kr>
2024-09-24 06:05:50 +0500

    live

--- python-dds.py
+++ python-dds.py
@@ -144,7 +144,7 @@ tur.tracer(20)
 tur.width(3)
 tur.bgcolor("black")
 for j in range(225):
-    for i in range(15): 
+    for i in range(15):
         tur.color(cs.hsv_to_rgb(i/15, j/25, 1))
         tur.right(90)
         tur.circle(200-j*4,90)
@@ -154,8 +154,23 @@ for j in range(225):
         tur.circle(50,24)
         tur.hideturtle()
         tur.done()
-        
 
 
-
-
+        import turtle as tur
+import colorsys as cs
+tur.setup(800, 800)
+tur.speed(10)
+tur.tracer(20)
+tur.width(3)
+tur.bgcolor("black")
+for j in range(225):
+    for i in range(15):
+        tur.color(cs.hsv_to_rgb(i/15, j/25, 1))
+        tur.right(90)
+        tur.circle(200-j*4,90)
+        tur.left(90)
+        tur.circle(200-j*4,90)
+        tur.right(180)
+        tur.circle(50,24)
+        tur.hideturtle()
+        tur.done()
