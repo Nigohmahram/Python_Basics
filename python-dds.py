bc10029519b129fccf9cdf198cc6d220a8318ab2
Khojiakbar <usmondev@outlook.kr>
2024-10-29 21:03:14 +0500

    refactor: clean code

--- python-dds.py
+++ python-dds.py
@@ -762,7 +762,19 @@ if __name__ == "__main__":
         )
     )
 
+# schedular code for blocking schedular as we have only 1 process to run
 
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+from caller import search
+
+
+sched = BlockingScheduler()
+
+# Schedule job_function to be called every two hours
+sched.add_job(search, "interval", hours=1)  # for testing instead add hours =1
+
+sched.start()
 
 
 
